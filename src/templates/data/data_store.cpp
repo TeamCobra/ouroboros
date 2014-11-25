f2ea7642e319de1bf3ef50b1ade64395008d8f22
Gabriel Marcano <gem5597@rit.edu>
2014-11-25 17:22:48 -0500

    Fleshed out basic idea for data_store.

---
+++ src/templates/data/data_store.cpp
@@ -0,0 +1,14 @@
+#include "data_store.hpp"
+
+namespace ouroboros
+{
+		data_store::data_store(group aRoot)
+		{
+			
+		}
+		
+		base_field *data_store::get(const std::string& aName, const std::string& aGroup)
+		{
+			
+		}
+}
--- src/templates/data/data_store.hpp
+++ src/templates/data/data_store.hpp
@@ -1,15 +1,20 @@
 #ifndef _OUROBOROS_DATASTORE_H_
 #define _OUROBOROS_DATASTORE_H_
 
+#include "base.hpp"
+#include <string>
+
 namespace ouroboros
 {
 	class data_store
 	{
 	public:
+		data_store(group aRoot);
+		base_field *get(const std::string& aName, const std::string& aGroup);
 		
 	private:
-		
-	}
+		group mRoot;
+	};
 
 }
 
