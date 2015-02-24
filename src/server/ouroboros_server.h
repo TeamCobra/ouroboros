8173fdfb443f58090243bb916f1ab8244af6666e
Gabriel Marcano <gem5597@rit.edu>
2015-02-24 00:20:47 -0500

    ouroboros_server documentation and threading.
    Added documentation for ouroboros_server, and modified it to support a
    single threaded mode of execution. main was updated to use this new
    mode.

--- src/server/ouroboros_server.h
+++ src/server/ouroboros_server.h
@@ -14,24 +14,83 @@ namespace ouroboros
 	class ouroboros_server
 	{
 	public:
+		/**	Constructor.
+		 * 
+		 *	Prepares server to run.
+		 * 
+		 */
 		ouroboros_server();
+		
+		/**	Destructor.
+		 *
+		 *	Unloads server resources.
+		 */
 		~ouroboros_server();
 		
-		void start();
-		void stop();
-		bool started();
+		/**	Executes one iteration of the server's main routine.
+		 * 
+		 *	This function should be called in a loop in some thread, like main.
+		 *	Each execution of the function causes the server to process its
+		 *	currently pending I/O requests. One way to use this function is to
+		 *	place it in the main function inside of a while loop with a
+		 *	signal variable controlling when to exit.
+		 *
+		 *	@post The server has executed all I/O requests that were pending
+		 *		previously.
+		 */
+		void run();
 		
+		//@{
+		/**	Returns a pointer to an element stored within the server's device
+		 *		tree structure.
+		 *
+		 *	@param [in] aGroup String describing the group of the desired
+		 *		element.
+		 *	@param [in] aField String describing the name of the desired field.
+		 *
+		 *	@returns A pointer to the desired element if found, NULL otherwise.
+		 *
+		 */
 		var_field *get(const std::string& aGroup, const std::string& aField);
-		group<var_field> *get(const std::string&);
+		const var_field *get(const std::string& aGroup, const std::string& aField) const;
+		//@}
 		
+		//@{
+		/**	Returns a pointer to a group structure stored within the server's
+		 *		device tree structure.
+		 *
+		 *	@param [in] aGroup String describing the group of the desired
+		 *		element.
+		 *
+		 *	@returns A pointer to the desired group if found, NULL otherwise.
+		 *
+		 */
+		group<var_field> *get(const std::string&);
+		const group<var_field> *get(const std::string&) const;
+		//@}
+		
+		/**	Sets the content of the desired element to a copy of the one
+		 *		specified.
+		 *FIXME I don't think this function is implemented correctly
+		 */
 		bool set(const std::string& aGroup, const std::string& aField, const var_field& aFieldData);
+		
+		//FIXME do we even want this function?
 		bool set(const std::string& aGroup, const group<var_field>& aField);
 		
+		/**	Registers a callback function for the specified element.
+		 *
+		 *	@param [in] aGroup String describing the group of the desired
+		 *		element.
+		 *	@param [in] aField String describing the name of the desired field.
+		 *	@param [in] aCallback Callback functor that is called as void()
+		 *		function.
+		 *	@returns True upon success, false otherwise.
+		 */
 		template <typename Func>
-		void register_callback(const std::string& aGroup, const std::string& aField, Func aCallback);
+		bool register_callback(const std::string& aGroup, const std::string& aField, Func aCallback);
 		
 	private:
-		static void* run_server(void*);
 		
 		mg_result handle_uri(mg_connection *conn, const std::string& uri);
 		mg_result handle_rest(const rest_request& request);
@@ -42,14 +101,15 @@ namespace ouroboros
 		static int event_handler(mg_connection *conn, mg_event ev);
 		static std::string normalize_group(const std::string& aGroup);
 		
+		//@{
+		//Do not allow for the server to be copyable nor allow for it to be
+		//assigned to anything else.
 		ouroboros_server(const ouroboros_server&);
 		ouroboros_server& operator=(const ouroboros_server&);
+		//@}
 		
 		mg_server *mpServer;
 		data_store<var_field>& mStore;
-		
-		bool mStarted;
-		pthread_t mThread;
 		
 		typedef void (*callback_function)(const std::string& aGroup, const std::string& aField);
 		std::map<std::string, subject<callback<callback_function> > > mCallbackSubjects;
