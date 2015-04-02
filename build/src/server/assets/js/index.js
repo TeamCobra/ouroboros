00d5b1196a6427387f3ae8e9e8bf572577c319ad
Jared Smith <jared.rit@gmail.com>
2015-04-01 21:34:29 -0400

    Add support for building the nav bar, inputs (they're all string fields), subgroups, and updating any input, whether nested or not

---
+++ build/src/server/assets/js/index.js
@@ -0,0 +1,212 @@
+$(document).ready(function () {
+  nav = new Nav();
+
+  // load in the tabs for navigation
+  makeNavBar(nav);
+
+  $(".navbar").click("a", getGroup);  
+  $(".navbar").click("a", nav.setActive);  
+  $("body").submit(".field-form", updateField);  
+});
+
+function updateField(e) {
+  e.preventDefault();
+
+  // get the form, its url, and value(theres only one)
+  var $form = $(e.target);
+  var url = $form.attr("action");
+  var updateValue = $form.find("input").val();
+
+  // submit an update request, refresh the page on done
+  $.ajax({
+    url: url,
+    type: "PUT",
+    contentType: "application/json",
+    data: JSON.stringify({ value: updateValue})
+  }).done(function(data) {
+    var groupId = $('.navbar li.active').data('group-id')
+    // refresh data by getting page info again and reloading the page
+    $.getJSON("/groups/" + groupId, function(data) { 
+      var rootTitle = $('.navbar li.active').data("group-id");
+      loadPage(data); 
+    });
+  });
+}
+
+function getGroup(e) {
+  // get the navigation link clicked
+  var li = $(e.target).parent()
+  var classes = li.attr("class");
+  // if its not active, change the content
+  if (classes == undefined || classes == "" || !classes.includes("active")) {
+    // pull off the group id for a GET request
+    var url = "/groups/" + li.data("group-id")
+    // get the json for the page and load it
+    $.getJSON(url, function(data) {
+      var rootTitle = $('.navbar li.active').data("group-id");
+      loadPage(data);
+    })
+  }
+}
+
+
+function makeNavBar(nav) { 
+  var index = 0;
+  // get the index json for groups, this is universal
+  // across all xml inputs so we can get all info
+  $.getJSON("/groups", function(json) {
+    // iterate through the first level depth 
+    $.each(json.contents, function(id, data) {
+      // if there is no value, its a group
+      if (data.value == undefined) {
+        // add it to the navigation section
+        nav.add(data.title, index, id);
+      } else {
+        // it its not a group add it as input
+        // to the main content section
+        var input = new Input(id, data);
+        $(".page").append(input.$el);
+      }
+      
+      // if this iteration is for the first menu item,
+      // make it active and load its contents
+      if (index == 0 && data.type == "group") {
+        nav.initActiv(0);
+        loadPage(data, id);
+      }
+      
+      index++;
+    });       
+  });
+}
+
+function loadPage(data, rootTitle) {
+  // clear whatever is in the main content area now
+  $(".page").html("");
+
+  // iterate through the elements in the page
+  $.each(data.contents, function(id, sectionData) {
+    // if its a group, add the section header
+    if (sectionData.type == "group") {
+      var rootTitle = $(".active").data("group-id")
+      var subGroup = new SubGroup(sectionData.title, id);
+
+      addSubGroup(id, sectionData, subGroup);
+      subGroup.buildUrls(rootTitle);
+    } else {
+      // for a field, build the url and put it in the input
+      var url = "/groups/" + $(".active").data("group-id") + "/fields/"
+      var input = new Input(id, sectionData, url);
+      $(".page").append(input.$el);
+    }
+  });
+}
+
+
+/* 
+ * Subgroup and Input related code
+ */
+
+// takes in the name and the contained data for a given subgroup
+// and adds inputs as well as recursively adding any extra subgroups
+function addSubGroup(name, data, parent) {
+  $.each(data.contents, function(id, contents) {
+    // if we have a nested group, recurse
+    if (contents.type == "group") {
+      var subGroup = new SubGroup(data.title, name);
+      parent.subgroups.push(subGroup);
+      addSubGroup(contents.title, contents, subGroup);
+    } else {
+      // if not a group, add input to the SubGroup object
+      parent.addInput(id, contents);
+    } 
+  })
+  $(".page").append(parent.$el);
+}
+
+// SubGroup holds some easy to access attributes as well as its DOM
+// element. This allows for us to go through after fully loading a 
+// page and adding the full urls easily
+function SubGroup(name, id) {
+  this.subgroups = [];
+  this.inputs = [];
+
+  this.name = name;
+  this.id = id;
+  
+  this.$el = $('<div class="panel panel-default"><div class="panel-heading">' + name + '</div><div class="panel-body"></div></div>');
+}
+
+
+// Handles making a new input and putting it in the subgroup's internal
+// inputs variable as well as in the DOM
+SubGroup.prototype.addInput = function(id, contents) {
+  var input = new Input(id, contents);
+  this.inputs.push(input);
+  this.$el.find('.panel-body').eq(0).append(input.$el);
+}
+
+SubGroup.prototype.buildUrls = function(url) {
+  base = url + "." + this.id;
+  $.each(this.inputs, function(i, input) {
+    var url = "/groups/" + base + "/fields/" + input.name;
+    input.$el.attr("action", url);
+  });
+
+  $.each(this.subgroups, function(i, subgroup) {
+    subgroup.buildUrls(base);
+  });
+}
+
+
+// Input is used to hold state of an input field and easily allow us to
+// hold it in a SubGroup for final page loading
+function Input(name, data, baseUrl) {
+  // make a copy of the hidden template in the DOM
+  this.$el = $($('#input-template').html()).clone()
+
+  // keep a copy of name for when building urls
+  this.name = name;
+  
+  // make the base URL and add it in
+  var url = baseUrl + name;
+  this.$el.attr("action", url);
+
+  // fill in information for the label
+  var $span = this.$el.find('#name-value-label')
+  var nameValueLabel = data.title + " : " + data.value
+  $span.text(nameValueLabel);
+}
+
+
+
+
+/*
+ * Nav Object Section and subsections
+ */
+
+function Nav() {
+  this.groupLinks = []
+}
+
+Nav.prototype.add = function(groupName, id, groupId) {
+  link = new GroupLink();
+  link.$el.find("a").text(groupName);
+  link.$el.attr("data-id", id);
+  link.$el.attr("data-group-id", groupId);
+  this.groupLinks.push(link);
+  $('.navbar').append(link.$el);
+}
+
+Nav.prototype.initActiv = function(i) {
+  this.groupLinks[i].$el.addClass("active");
+}
+
+Nav.prototype.setActive = function(e) {
+  $(".navbar li").removeClass("active");
+  $(e.target.parentElement).addClass("active");
+}
+
+function GroupLink() {
+  this.$el = $('<li role="presentation"><a href="#"></a></li>');
+}
