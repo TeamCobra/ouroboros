0048aca93b2c4ffc6df9fcaa5745a277cf96b046
jkugs <jkuglics@gmail.com>
2015-04-11 17:08:03 -0400

    Fixed indentation.

--- src/logic_checker.rb
+++ src/logic_checker.rb
@@ -28,19 +28,22 @@ class LogicChecker
 		if defined? field.min and not field.min.empty?
 			!decimal ? min = field.min.chardata[0].strip.to_i : min = field.min.chardata[0].strip.to_f
 			if min < absoluteMin
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nmin: #{min} is outside of range " + 
-							"for data type #{field.type}\n#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"min: #{min} is outside of range for data type #{field.type}\n" +
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
 				exit 1
 			end
 			if value < min
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nvalue: #{value} is lower than the assigned min" +
-							"\nmin: #{min}\n\n"				
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"value: #{value} is lower than the assigned min\n" +
+							"min: #{min}\n\n"				
 				exit 1
 			end
 		else
 			if value < absoluteMin
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nvalue: #{value} is outside of range " + 
-							"for data type #{field.type}\n#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" + 
+							"value: #{value} is outside of range for data type #{field.type}\n" +
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
 				exit 1
 			end
 		end
@@ -48,19 +51,22 @@ class LogicChecker
 		if defined? field.max and not field.max.empty?
 			!decimal ? max = field.max.chardata[0].strip.to_i : max = field.max.chardata[0].strip.to_f 
 			if max > absoluteMax
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nmax: #{max} is outside of range " + 
-							"for data type #{field.type}\n#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"max: #{max} is outside of range for data type #{field.type}\n" +
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
 				exit 1
 			end
 			if value > max
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nvalue: #{value} is greater than the assigned max" +
-							"\nmax: #{max}\n\n"
-							exit 1
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"value: #{value} is greater than the assigned max\n" +
+							"max: #{max}\n\n"
+				exit 1
 			end
 		else
 			if value > absoluteMax
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nvalue: #{value} is outside of range " + 
-							"for data type #{field.type}\n#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"value: #{value} is outside of range for data type #{field.type}\n" +
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
 				exit 1
 			end
 		end
@@ -71,8 +77,9 @@ class LogicChecker
 		if defined? field.minLength and not field.minLength.empty?
 			minLength = field.minLength.chardata[0].strip.to_i
 			if value.length < minLength
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nvalue: \"#{value}\" length (#{value.length}) "+
-							"is lower than the assigned minlength \nminLength: #{minLength}\n\n"
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"value: \"#{value}\" length (#{value.length}) is lower than the assigned minlength\n" +
+							"minLength: #{minLength}\n\n"
 				exit 1
 			end
 		end
@@ -80,16 +87,18 @@ class LogicChecker
 		if defined? field.maxLength and not field.maxLength.empty?
 			maxLength = field.maxLength.chardata[0].strip.to_i
 			if value.length > maxLength
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nvalue: \"#{value}\" length (#{value.length}) "+
-							"is greater than the assigned maxlength \nmaxLength: #{maxLength}\n\n"
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"value: \"#{value}\" length (#{value.length}) is greater than the assigned maxlength\n" +
+							"maxLength: #{maxLength}\n\n"
 				exit 1
 			end
 		end
 
 		if defined? field.pattern and not field.pattern.empty?
 			if not value =~ /#{field.pattern.chardata[0].strip}/
-				puts "\nERROR in field: #{field.title.chardata[0].strip}\nvalue: \"#{value}\" does not match assigned regex pattern"+
-							"\npattern: #{field.pattern.chardata[0].strip}\n\n"
+				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
+							"value: \"#{value}\" does not match assigned regex pattern\n" +
+							"pattern: #{field.pattern.chardata[0].strip}\n\n"
 				exit 1
 			end
 		end
