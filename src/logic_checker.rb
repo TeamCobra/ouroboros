2f12e3cfa64656a28efe2a2722b4c64e3542908b
jkugs <jkuglics@gmail.com>
2015-05-20 21:26:18 -0400

    Modified schema and validation process of XML.

--- src/logic_checker.rb
+++ src/logic_checker.rb
@@ -3,6 +3,8 @@ require './ranges'
 class LogicChecker
 	NUM_TYPES = ["intField", "signedIntField", "unsignedIntField", "signedShortField",
 		"unsignedShortField", "signedByteField", "unsignedByteField", "booleanField"]
+	
+	@@logical = true
 
 	def checkLogic(model)
 		model.field.each do |field|
@@ -16,7 +18,8 @@ class LogicChecker
 		end
 		model.group.each do |group|
 			checkLogic(group)
-		end	
+		end
+		return @@logical	
 	end
 
 	def checkNumFieldLogic(field, decimal)
@@ -25,81 +28,81 @@ class LogicChecker
 		
 		!decimal ? value = field.value.chardata[0].to_i : value = field.value.chardata[0].to_f
 		
-		if defined? field.min and not field.min.empty?
+		if field.min.size > 0
 			!decimal ? min = field.min.chardata[0].strip.to_i : min = field.min.chardata[0].strip.to_f
 			if min < absoluteMin
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"min: #{min} is outside of range for data type #{field.type}\n" +
-							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
-				exit 1
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n"
+				@@logical = false;
 			end
 			if value < min
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"value: #{value} is lower than the assigned min\n" +
-							"min: #{min}\n\n"				
-				exit 1
+							"min: #{min}\n"				
+				@@logical = false;
 			end
 		else
 			if value < absoluteMin
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" + 
 							"value: #{value} is outside of range for data type #{field.type}\n" +
-							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
-				exit 1
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n"
+				@@logical = false;
 			end
 		end
 		
-		if defined? field.max and not field.max.empty?
+		if field.max.size > 0
 			!decimal ? max = field.max.chardata[0].strip.to_i : max = field.max.chardata[0].strip.to_f 
 			if max > absoluteMax
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"max: #{max} is outside of range for data type #{field.type}\n" +
-							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
-				exit 1
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n"
+				@@logical = false
 			end
 			if value > max
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"value: #{value} is greater than the assigned max\n" +
-							"max: #{max}\n\n"
-				exit 1
+							"max: #{max}\n"
+				@@logical = false;
 			end
 		else
 			if value > absoluteMax
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"value: #{value} is outside of range for data type #{field.type}\n" +
-							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n\n"
-				exit 1
+							"#{field.type} range: #{absoluteMin} - #{absoluteMax}\n"
+				@@logical = false;
 			end
 		end
 	end
 
 	def checkStringField(field)
 		value = field.value.chardata[0].strip
-		if defined? field.minLength and not field.minLength.empty?
+		if field.minLength.size > 0
 			minLength = field.minLength.chardata[0].strip.to_i
 			if value.length < minLength
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"value: \"#{value}\" length (#{value.length}) is lower than the assigned minlength\n" +
-							"minLength: #{minLength}\n\n"
-				exit 1
+							"minLength: #{minLength}\n"
+				@@logical = false
 			end
 		end
 
-		if defined? field.maxLength and not field.maxLength.empty?
+		if field.maxLength.size > 0
 			maxLength = field.maxLength.chardata[0].strip.to_i
 			if value.length > maxLength
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"value: \"#{value}\" length (#{value.length}) is greater than the assigned maxlength\n" +
-							"maxLength: #{maxLength}\n\n"
-				exit 1
+							"maxLength: #{maxLength}\n"
+				@@logical = false
 			end
 		end
 
-		if defined? field.pattern and not field.pattern.empty?
+		if field.pattern.size > 0
 			if not value =~ /#{field.pattern.chardata[0].strip}/
 				puts "\nERROR in field: #{field.title.chardata[0].strip}\n" +
 							"value: \"#{value}\" does not match assigned regex pattern\n" +
-							"pattern: #{field.pattern.chardata[0].strip}\n\n"
-				exit 1
+							"pattern: #{field.pattern.chardata[0].strip}\n"
+				@@logical = false
 			end
 		end
 	end
--- src/server_model.rb
+++ src/server_model.rb
@@ -1,9 +1,29 @@
 module ServerModel
   extend RGen::MetamodelBuilder::ModuleExtension
   
-  class Field < RGen::MetamodelBuilder::MMBase; end
+	class Title < RGen::MetamodelBuilder::MMBase; end
+	class Description < RGen::MetamodelBuilder::MMBase; end
+	class Value < RGen::MetamodelBuilder::MMBase; end
+	class Min < RGen::MetamodelBuilder::MMBase; end
+	class Max < RGen::MetamodelBuilder::MMBase; end
+	class MinLength < RGen::MetamodelBuilder::MMBase; end
+	class MaxLength < RGen::MetamodelBuilder::MMBase; end
+	class Pattern < RGen::MetamodelBuilder::MMBase; end
+
+  class Field < RGen::MetamodelBuilder::MMBase 
+		contains_many 'title', Title, 'parent'
+		contains_many 'description', Description, 'parent'
+		contains_many 'value', Value, 'parent'
+		contains_many 'min', Min, 'parent'
+		contains_many 'max', Max, 'parent'
+		contains_many 'minLength', MinLength, 'parent'
+		contains_many 'maxLength', MaxLength, 'parent'
+		contains_many 'pattern', Pattern, 'parent'
+	end
   
   class Group < RGen::MetamodelBuilder::MMBase
+    contains_many 'title', Title, 'parent'
+    contains_many 'description', Description, 'parent'
     contains_many 'group', Group, 'parent'
     contains_many 'field', Field, 'parent'
   end
